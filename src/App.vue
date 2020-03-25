3e543cb01fa83c478a965eb6a7263b07366b5bed
manyuet <907878374@qq.com>
2020-03-25 17:02:19 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ol-map />
+  </div>
+</template>
+
+<script>
+import OlMap from "./components/OlMap";
+
+export default {
+  name: 'App',
+  components: {
+    OlMap
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100%;
+}
+  *{
+    padding: 0;
+    margin: 0;
+  }
+  html,body{
+    height: 100%;
+  }
+</style>
