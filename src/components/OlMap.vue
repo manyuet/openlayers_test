2d1f4c0ffadbf447fd3df5d045baa8aa268ee0b8
manyuet <907878374@qq.com>
2020-03-27 17:47:09 +0800

    test

--- src/components/OlMap.vue
+++ src/components/OlMap.vue
@@ -5,35 +5,45 @@
     <!--    <button @click="addLayer">addLayer</button>-->
     <!--    <button @click="addLayer1">addLayers</button>-->
     <!--    <button @click="display">display</button>-->
-    <input type="radio" checked="checked" name="mapSource" @click="switchToOSM">OpenStreetMap地图
+    <input type="radio" name="mapSource" @click="switchToOSM">OpenStreetMap地图
     <input type="radio" name="mapSource" @click="switchToBing">Bing地图
     <input type="radio" name="mapSource" @click="switchToStamen">Stamen地图
+    <button @click="addRoadMap">roadMapOn</button>
   </div>
 </template>
 
 <script>
 import 'ol/ol.css'
 import { Map, View } from 'ol'
+// import Feature from 'ol/Feature'
 import TileLayer from 'ol/layer/Tile'
+// import Vector from 'ol/layer/Vector'
+// import SVector from 'ol/source/Vector'
 import OSM from 'ol/source/OSM'
 import BingMaps from 'ol/source/BingMaps'
 import Stamen from 'ol/source/Stamen'
-// import XYZ from 'ol/source/XYZ'
+// import GeoJSON from 'ol/format/GeoJSON'
+import XYZ from 'ol/source/XYZ'
+// import Point from 'ol/geom/Point'
 // import { fromLonLat } from 'ol/proj.js'
 export default {
   name: 'OlMap',
   data() {
     return {
-      map: null
+      map: null,
+      roadMap: false
     }
   },
   mounted() {
     this.initMap()
   },
   methods: {
+    test() {
+      console.log(2)
+    },
     initMap() {
       const view = new View({ // 地图视图
-        center: [104.06, 30.67], // 坐标
+        center: [104, 30], // 坐标
         zoom: 10,
         projection: 'EPSG:4326'
       })
@@ -41,7 +51,10 @@ export default {
         target: 'map', // 对应页面里id为map的元素
         layers: [// 图层
           new TileLayer({
-            source: new OSM()
+            source: new XYZ({
+              url: 'http://t4.tianditu.com/DataServer?T=vec_w&x={x}&y={y}&l={z}&tk=240859a554196e2374a6bb80cfdd3de6',
+              zIndex: 50
+            })
           })
         ],
         view: view
@@ -51,8 +64,8 @@ export default {
       var openStreetMapLayer = new TileLayer({
         source: new OSM()
       })
-      this.map.removeLayer(this.map.getLayers().item(0))
-      this.map.addLayer(openStreetMapLayer)
+      this.map.removeLayer(this.map.getLayers().item(0)) // 移除给定的图层
+      this.map.addLayer(openStreetMapLayer) // 将给定的图层添加到地图顶部
     },
     switchToBing() {
       var bingMapLayer = new TileLayer({
@@ -72,8 +85,21 @@ export default {
       })
       this.map.removeLayer(this.map.getLayers().item(0))
       this.map.addLayer(stamentLayer)
+    },
+    addRoadMap() {
+      var tiandituLuWang = new TileLayer({
+        source: new XYZ({
+          url: 'http://t3.tianditu.com/DataServer?T=cva_w&x={x}&y={y}&l={z}&tk=240859a554196e2374a6bb80cfdd3de6'
+        }),
+        zIndex: 200
+      })
+      this.roadMap = !this.roadMap
+      if (this.roadMap) {
+        this.map.addLayer(tiandituLuWang) // 将给定的图层添加到地图顶部
+      } else {
+        this.map.removeOverlay(tiandituLuWang)
+      }
     }
-
     // display() {
     //   this.map.getView().fit([104, 30.6, 104.12, 30.74], this.map.getSize())
     // },
